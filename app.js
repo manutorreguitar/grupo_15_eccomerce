4dac053ddfc3e14c075855ba3aa6f1545bfffeab
AxelFoleyArg <odurninaxel@gmail.com>
2022-11-06 16:58:10 -0300

    App, Home, Login y Register vinculados

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require ('express');
-const app = express ();
-const path = require ('path')
-let PORT = 3000
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
-app.use (express.static('views'))
-app.use (express.static('public'))
+let PORT = process.env.PORT || 3000
 
-app.listen (PORT, console.log("Listen on PORT " + PORT));
+app.listen(PORT, ()=>{
+    console.log('Funcionando en el Puerto ' + '');
+});
 
 app.get('/', (req,res)=>{
-    res.sendFile (path.resolve ('./views/home.html'))
+    res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + ('./views/login.html'));
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + ('./views/register.html'));
+    res.sendFile(__dirname + '/views/register.html');
 });
